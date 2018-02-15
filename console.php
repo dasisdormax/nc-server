73d884b4765096defc6e155feaaf1699c9e18a8a
Joas Schilling <coding@schilljs.com>
2018-02-15 16:20:00 +0100

    Test3
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- console.php
+++ console.php
@@ -37,7 +37,7 @@ use Symfony\Component\Console\Output\ConsoleOutput;
 
 define('OC_CONSOLE', 1);
 
-function exceptionHandler(Error $exception) {
+function exceptionHandler($exception) {
 
 	echo "An unhandled exception has been thrown:" . PHP_EOL;
 	echo $exception;
